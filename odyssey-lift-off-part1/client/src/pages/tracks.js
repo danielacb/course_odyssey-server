135e764bee52b11a68d946033daddc6bf9b70b5d
Daniela Barbosa <contato@danielacb.com>
2022-05-03 22:39:50 -0300

    Lift-off I: code the client side and finish the course

--- odyssey-lift-off-part1/client/src/pages/tracks.js
+++ odyssey-lift-off-part1/client/src/pages/tracks.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
+import { gql, useQuery } from "@apollo/client";
+
+const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      length
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      url
+      modulesCount
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { data, error, loading } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
